3ae5ceee6ab493bbde788ee0d46cf3b55bcc8d4f
Mora <mohanad1481979@gmail.com>
2025-08-10 00:08:35 +0300

    custom drawer done

---
+++ lib/widgets/adaptive_layout_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AdaptiveLayout extends StatelessWidget {
+  const AdaptiveLayout(
+      {super.key,
+      required this.mobileLayout,
+      required this.tabletLayout,
+      required this.desktopLayout});
+
+  final WidgetBuilder mobileLayout, tabletLayout, desktopLayout;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constrains) {
+        if (constrains.maxWidth < 600) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: mobileLayout(context),
+          );
+        } else if (constrains.maxWidth < 900) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: tabletLayout(context),
+          );
+        } else {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: desktopLayout(context),
+          );
+        }
+      },
+    );
+  }
+}
---
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+
+class DashBoardDesktopLayout extends StatelessWidget {
+  const DashBoardDesktopLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: CustomDrawer(),
+        ),
+      ],
+    );
+  }
+}
