d41db681eb1a410a0c07c3740ac032308a24afd9
Mora <mohanad1481979@gmail.com>
2025-08-12 11:10:48 +0300

    create latest transaction and their list view

---
+++ lib/widgets/latest_transaction.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
+import 'package:responsive_dash_board/widgets/latest_transaction_list_view.dart';
+
+class LatestTransaction extends StatelessWidget {
+  const LatestTransaction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Latest Transaction',
+          style: AppStyles.styleMedium16(context),
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        LatestTransactionListView()
+      ],
+    );
+  }
+}
+
+
