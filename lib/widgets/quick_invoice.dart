03e4f89b2f5160e0c618b06c4a00a08975ad5ed8
Mora <mohanad1481979@gmail.com>
2025-08-13 10:11:27 +0300

    create custom text field widget

--- lib/widgets/quick_invoice.dart
+++ lib/widgets/quick_invoice.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/custom_background_container.dart';
 import 'package:responsive_dash_board/widgets/latest_transaction.dart';
 import 'package:responsive_dash_board/widgets/quick_invoice_header.dart';
+import 'package:responsive_dash_board/widgets/title_text_field.dart';
 
 class QuickInvoice extends StatelessWidget {
   const QuickInvoice({super.key});
@@ -20,6 +21,7 @@ class QuickInvoice extends StatelessWidget {
             height: 48,
 
           ),
+          TitleTextField(title: 'Customer name', hint: 'Type customer name')
         ],
       ),
     );
