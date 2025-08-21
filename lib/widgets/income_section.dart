91b7e6ddafd2b79e3111c216c4d7b6c58e9a61fc
Mora <mohanad1481979@gmail.com>
2025-08-21 12:42:08 +0300

    refactor to handel income section ui issue

--- lib/widgets/income_section.dart
+++ lib/widgets/income_section.dart
@@ -2,8 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:responsive_dash_board/widgets/custom_background_container.dart';
-import 'package:responsive_dash_board/widgets/income_chart.dart';
-import 'package:responsive_dash_board/widgets/income_details.dart';
+import 'package:responsive_dash_board/widgets/income_section_body.dart';
 import 'package:responsive_dash_board/widgets/income_secton_header.dart';
 
 class IncomeSection extends StatelessWidget {
@@ -15,13 +14,7 @@ class IncomeSection extends StatelessWidget {
       child: Column(
         children: [
           IncomeSectionHeader(),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(child: IncomeChart()),
-              Expanded(child: IncomeDetails()),
-            ],
-          )
+          IncomeSectionBody(),
         ],
       ),
     );
