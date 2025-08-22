11827f56e267d28ceb38d6a9d96b2ba2066ff653
Mora <mohanad1481979@gmail.com>
2025-08-22 13:45:50 +0300

    project is done

--- lib/widgets/active_and_inactive_item.dart
+++ lib/widgets/active_and_inactive_item.dart
@@ -39,9 +39,12 @@ class ActiveDrawerItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       leading: SvgPicture.asset(drawerItemModel.image),
-      title: Text(
-        drawerItemModel.title,
-        style: AppStyles.styleBold16(context),
+      title: FittedBox(
+        fit: BoxFit.scaleDown,
+        child: Text(
+          drawerItemModel.title,
+          style: AppStyles.styleBold16(context),
+        ),
       ),
       trailing: Container(
         width: 3.27,
